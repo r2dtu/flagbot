1235ba102774019adf2dc588038be2e746a24063
r2dtu <dctu@ucsd.edu>
2021-05-31 12:02:51 -0700

    Added dynamic help messages

--- flagbot.js
+++ flagbot.js
@@ -13,8 +13,8 @@ const { prefix, token } = require( './config.json' );
 client.commands = new Discord.Collection();
 const cmdFiles = fs.readdirSync( './commands' ).filter(file => file.endsWith( '.js' ));
 for (const file of cmdFiles) {
-  const cmd = require(`./commands/${file}`);
-  client.commands.set( cmd.name, cmd );
+    const cmd = require( `./commands/${file}` );
+    client.commands.set( cmd.name, cmd );
 }
 
 // Ready message after startup
@@ -24,18 +24,44 @@ client.once( 'ready', () => {
 
 // Messages/commands handler
 client.on( 'message', msg => {
+
     // Ignore bot and unprefixed commands
     if (!msg.content.startsWith( prefix ) || msg.author.bot) return;
   
     // Get rid of prefix and separate arguments passed in
     const args = msg.content.slice( prefix.length ).trim().split(/ +/);
-    const cmd = args.shift().toLowerCase();
+    const cmdName = args.shift().toLowerCase();
+
+    const cmd = client.commands.get( cmdName )
+        || client.commands.find( c => c.aliases && c.aliases.includes( cmdName ) );
   
     // If command does not exist, just ignore it
-    if (!client.commands.has( cmd )) return;
-    
+    if (!cmd) return;
+
+    // Ensure command can only be used where it can
+    if (cmd.guildOnly && msg.channel.type === 'dm') {
+        return msg.reply('I can\'t execute that command inside DMs!');
+    }
+
+    // Ensure user has permissions to use command
+    if (cmd.permissions) {
+        const authorPerms = msg.channel.permissionsFor( msg.author );
+        if (!authorPerms || !authorPerms.has( cmd.permissions )) {
+            return msg.reply( 'You are not authorized to use this command.' );
+        }
+    }
+
+    // Check that user inputted an argument
+    if (cmd.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+        if (cmd.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+        return msg.channel.send( reply );
+    }
+
     try {
-        client.commands.get( cmd ).execute( msg, args );
+        cmd.execute( msg, args );
     } catch (e) {
         console.error( e );
         msg.reply( 'There was an error trying to execute that command!' );
